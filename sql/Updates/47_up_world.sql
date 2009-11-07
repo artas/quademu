b93aa2175cddd563f9ec605d60bb30e06167c387
Bazilio <BazilioRn@gmail.com>
2009-11-07 21:42:23 +0300

    rev[47]Naxxramas, Grand Widow Faerlina: script adds.

---
+++ sql/Updates/47_up_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `ScriptName`='mob_faerlina_add' WHERE `entry`=16506;
+
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -7,7 +7,7 @@
 #endif // __REVISION_H__
 
 #ifndef __IN_T__
-#define _D "46"
+#define _D "47"
 #endif //__IN_T__
 
 
